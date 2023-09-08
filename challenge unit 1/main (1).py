8142516141731aaa5fcb3531431ef7b152e48788
asanm30722214015 <144338800+asanm30722214015@users.noreply.github.com>
2023-09-08 16:27:31 +0530

    Add files via upload

---
+++ challenge unit 1/main (1).py
@@ -0,0 +1,14 @@
+  # Leap Year
+
+def isLeapYear (Year):
+   if (Year % 4 == 0 and Year % 100 != 0 ) or Year % 400 == 0:
+    return True 
+   else:
+    return False
+
+Year = int (input("Enter a Year : "))
+if isLeapYear (Year):
+  print(' {} is a Leap year.'.format (Year))
+else:            
+  print(' {} is not a leap year.'.format (Year))
+
